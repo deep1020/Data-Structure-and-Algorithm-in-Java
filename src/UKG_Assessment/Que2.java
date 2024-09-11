f488fc6855b9ae3ea27b5873c6143fd7ac1dfb17
Deep Patel <drp037@gmail.com>
2024-09-10 17:48:03 -0700

    Commit

--- src/UKG_Assessment/Que2.java
+++
@@ -1,7 +0,0 @@
-package UKG_Assessment;
-
-public class Que2 {
-    public static void main(String[] args) {
-
-    }
-}
--- src/ass1.java
+++
@@ -1,9 +0,0 @@
-//public class ass1 {
-//    static String solution(String[] records){
-//
-//    }
-//
-//    public static void main(String[] args) {
-//
-//    }
-//}
